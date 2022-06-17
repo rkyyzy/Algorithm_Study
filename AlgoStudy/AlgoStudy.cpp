bc1aea014662df5e440570a92ac67a8bb6e7bc11
yzy98 <yzy98@students.cs.ubc.ca>
2022-06-17 11:07:40 -0700

    Add project files.

---
+++ AlgoStudy/AlgoStudy.cpp
@@ -0,0 +1,13 @@
+// AlgoStudy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#define cout std::cout
+#define endl std::endl
+
+int main()
+{
+	// C++ 17
+    // cout << _MSVC_LANG << endl;
+	return 0;
+}
